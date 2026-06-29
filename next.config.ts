cecf556049d3a19344d676db700893bde82e220e
tubetarou1135 <tubetarou1135@gmail.com>
2026-06-30 02:37:43 +0900

    fix: allow Rakuten image domains in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'thumbnail.image.rakuten.co.jp' },
+      { protocol: 'https', hostname: '*.image.rakuten.co.jp' },
+    ],
+  },
 };
 
 export default nextConfig;
